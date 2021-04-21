2fb359ec279a77a304e67091aeae434a9fa27e19
yutian.liu <939029835@qq.com>
2021-04-21 16:40:50 +0800

    新增run.n支持

--- src/Run.hx
+++ src/Run.hx
@@ -1,3 +1,5 @@
+import haxe.Exception;
+import sys.io.File;
 import python.FileUtils;
 import sys.FileSystem;
 
@@ -12,6 +14,61 @@ class Run {
 					FileUtils.copyDic(file, to);
 				} else {
 					FileUtils.copyFile(file, to);
+				}
+			case "rm":
+				var del = Sys.args()[1];
+				var ext = Sys.args()[2];
+				trace("rm " + del + " " + ext);
+				if (ext.indexOf("*") == 0) {
+					// 批量删除逻辑
+					ext = StringTools.replace(ext, "*", "");
+					if (FileSystem.isDirectory(del)) {
+						var files = FileSystem.readDirectory(del);
+						for (file in files) {
+							if (!FileSystem.isDirectory(file) && file.indexOf(ext) != -1) {
+								try {
+									FileSystem.deleteFile(del + "/" + file);
+									trace("Delete:", file);
+								} catch (e:Exception) {}
+							}
+						}
+					} else {
+						throw "这不是一个文件夹";
+					}
+				} else {
+					trace("Delete:", del);
+					// 单文件删除
+					if (FileSystem.isDirectory(del)) {
+						FileUtils.removeDic(del);
+					} else {
+						FileSystem.deleteFile(del);
+					}
+				}
+			case "bytes":
+				// 检查文件是否为二进制
+				var dir:String = Sys.args()[1];
+				var exxt:String = Sys.args()[2];
+				var files = FileSystem.readDirectory(dir);
+				for (file in files) {
+					if (file.indexOf(exxt) != -1) {
+						if(FileUtils.isBinary(dir + "/" + file)){
+							trace("二进制文件：" + file);
+						}
+					}
+				}
+			case "sign":
+				// 签名文本比较
+				var file:String = Sys.args()[1];
+				var array = File.getContent(file).split("\n");
+				var text1 = array[0];
+				var text2 = array[1];
+				for (i in 0...text1.length) {
+					var a1 = text1.charAt(i);
+					var a2 = text2.charAt(i);
+					if(a1 != a2){
+						trace("line=",i,text1.substr(0,i));
+						break;
+					}
 				}
 		}
 	}
--- src/python/FileUtils.hx
+++ src/python/FileUtils.hx
@@ -4,129 +4,118 @@ import sys.FileSystem;
 import sys.io.File;
 
 class FileUtils {
+	public static function createDir(dir:String):Void {
+		if (!FileSystem.exists(dir)) {
+			FileSystem.createDirectory(dir);
+		}
+	}
 
-    public static function createDir(dir:String):Void
-    {
-        if(!FileSystem.exists(dir))
-        {
-            FileSystem.createDirectory(dir);
-        }
-    }
+	public static function copyFile(file:String, copyTo:String):Void {
+		if (!FileSystem.exists(file)) {
+			trace("copyDic 路径不存在：" + file);
+			return;
+		}
+		var dir:String = copyTo.indexOf("/") == -1 ? copyTo : copyTo.substr(0, copyTo.lastIndexOf("/"));
+		if (!FileSystem.exists(dir)) {
+			FileSystem.createDirectory(dir);
+		}
+		if (FileSystem.exists(copyTo) && !FileSystem.isDirectory(copyTo))
+			FileSystem.deleteFile(copyTo);
+		File.copy(file, copyTo);
+	}
 
-    public static function copyFile(file:String,copyTo:String):Void
-    {
-        if(!FileSystem.exists(file)){
-            trace("copyDic 路径不存在："+file);
-            return;
-        }
-        var dir:String = copyTo.substr(0,copyTo.lastIndexOf("/"));
-        if(!FileSystem.exists(dir))
-        {
-            FileSystem.createDirectory(dir);
-        }
-        if(FileSystem.exists(copyTo) && !FileSystem.isDirectory(copyTo))
-            FileSystem.deleteFile(copyTo);
-        File.copy(file,copyTo);
-    }
+	/**
+	 * 拷贝文件夹
+	 * @param path
+	 * @param copyTo
+	 * @param igone 可在这里设置多个忽略文件
+	 */
+	public static function copyDic(dic:String, copyTo:String, igone:Array<String> = null):Void {
+		if (!FileSystem.exists(dic)) {
+			trace("copyDic 路径不存在：" + dic);
+			return;
+		}
+		var dicName:String = dic.indexOf("/") == -1 ? dic : dic.substr(dic.lastIndexOf("/") + 1);
+		if(!FileSystem.exists(copyTo + "/" + dicName))
+			FileSystem.createDirectory(copyTo + "/" + dicName);
+		var list:Array<String> = FileSystem.readDirectory(dic);
+		for (file in list) {
+			var file2:String = dic + "/" + file;
+			if (FileSystem.isDirectory(file2)) {
+				copyDic(file2, copyTo + "/" + dicName, igone);
+			} else {
+				if (igone == null || !getIsIgone(file, igone)) {
+					try {
+						File.copy(file2, copyTo + "/" + dicName);
+					} catch (e:Dynamic) {
+						trace("Warring:" + file2, e);
+					}
+				}
+			}
+		}
+	}
 
-    /**
-     * 拷贝文件夹
-     * @param path 
-     * @param copyTo
-     * @param igone 可在这里设置多个忽略文件 
-     */
-    public static function copyDic(dic:String,copyTo:String,igone:Array<String> = null):Void
-    {
-        if(!FileSystem.exists(dic)){
-            trace("copyDic 路径不存在："+dic);
-            return;
-        }
-        var dicName:String = dic.substr(dic.lastIndexOf("/") + 1);
-        FileSystem.createDirectory(copyTo + "/" + dicName);
-        var list:Array<String> = FileSystem.readDirectory(dic);
-        for(file in list)
-        {
-            var file2:String = dic + "/" + file;
-            if(FileSystem.isDirectory(file2))
-            {
-                copyDic(file2,copyTo + "/" + dicName,igone);
-            }
-            else
-            {
-                if(igone == null || !getIsIgone(file,igone))
-                {
-                    try{
-                        File.copy(file2,copyTo + "/" + dicName);
-                    }
-                    catch(e:Dynamic)
-                    {
-                        trace("Warring:"+file2,e);
-                    }
-                }
-            }
-        }
-    }
+	private static function getIsIgone(file:String, igone:Array<String>):Bool {
+		for (i in igone) {
+			if (file.indexOf(i) != -1)
+				return true;
+		}
+		return false;
+	}
 
-    private static function getIsIgone(file:String,igone:Array<String>):Bool
-    {
-        for(i in igone)
-        {
-            if(file.indexOf(i) != -1)
-                return true;
-        }
-        return false;
-    }
+	/**
+	 * 删除文件夹
+	 * @param dic
+	 */
+	public static function removeDic(dic:String):Void {
+		if (!FileSystem.exists(dic))
+			return;
+		var list:Array<String> = FileSystem.readDirectory(dic);
+		for (file in list) {
+			file = dic + "/" + file;
+			if (FileSystem.isDirectory(file)) {
+				removeDic(file);
+			} else {
+				FileSystem.deleteFile(file);
+			}
+		}
+		FileSystem.deleteDirectory(dic);
+	}
 
-    /**
-     * 删除文件夹
-     * @param dic 
-     */
-    public static function removeDic(dic:String):Void
-    {
-        if(!FileSystem.exists(dic))
-            return;
-        var list:Array<String> = FileSystem.readDirectory(dic);
-        for(file in list)
-        {
-            file = dic + "/" + file;
-            if(FileSystem.isDirectory(file))
-            {
-                removeDic(file);
-            }
-            else
-            {
-                FileSystem.deleteFile(file);
-            }
-        }
-        FileSystem.deleteDirectory(dic);
-    }
+	/**
+	 * 删除空文件夹
+	 * @param dic
+	 */
+	public static function removeNoneDic(dic:String):Bool {
+		if (!FileSystem.exists(dic))
+			return false;
+		var isCanDelDic:Bool = true;
+		var list:Array<String> = FileSystem.readDirectory(dic);
+		for (file in list) {
+			file = dic + "/" + file;
+			if (FileSystem.isDirectory(file)) {
+				if (!removeNoneDic(file))
+					isCanDelDic = false;
+			} else {
+				isCanDelDic = false;
+			}
+		}
+		if (isCanDelDic)
+			FileSystem.deleteDirectory(dic);
+		return isCanDelDic;
+	}
 
-    /**
-     * 删除空文件夹
-     * @param dic 
-     */
-    public static function removeNoneDic(dic:String):Bool
-    {
-        if(!FileSystem.exists(dic))
-            return false;
-        var isCanDelDic:Bool = true;
-        var list:Array<String> = FileSystem.readDirectory(dic);
-        for(file in list)
-        {
-            file = dic + "/" + file;
-            if(FileSystem.isDirectory(file))
-            {
-                if(!removeNoneDic(file))
-                    isCanDelDic = false;
-            }
-            else
-            {
-                isCanDelDic = false;
-            }
-        }
-        if(isCanDelDic)
-            FileSystem.deleteDirectory(dic);
-        return isCanDelDic;
-    }
-
+	/**
+	 * 判断文件是否为二进制
+	 * @param file
+	 * @return Bool
+	 */
+	public static function isBinary(file:String):Bool {
+		if (FileSystem.isDirectory(file))
+			return false;
+		var bytes = File.getBytes(file);
+		if (bytes.get(0) == 0 && bytes.get(1) == 0 && bytes.get(2) == 0 && bytes.get(3) == 0)
+			return true;
+		return false;
+	}
 }
